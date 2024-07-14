2771db3c0202f36fc04a03a3b5ced34b8b67a2e7
Denis Bondar <60221683+DenDunno@users.noreply.github.com>
2024-07-13 18:49:52 -0700

    release

--- Application/SecureData/SecureData.cs
+++ Application/SecureData/SecureData.cs
@@ -2,7 +2,7 @@ namespace Application.SecureData;
 
 public class SecureData
 {
-    public string DatabaseConnectionString { get; set; }
-    public string TelegramBotToken { get; set; }
-    public string AdminPassword { get; set; }
+    public string DatabaseConnectionString { get; set; } = null!;
+    public string TelegramBotToken { get; set; } = null!;
+    public string AdminPassword { get; set; } = null!;
 }
